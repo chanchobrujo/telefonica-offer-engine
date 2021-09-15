bfea6a57db40f68063b6ff550de4a4b92abed9e3
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-14 23:31:36 -0500

    Add services offer

---
+++ src/main/java/com/telefonica/offerengine/Service/OfferService.java
@@ -0,0 +1,58 @@
+package com.telefonica.offerengine.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.telefonica.offerengine.Data.Offer;
+import com.telefonica.offerengine.Interface.OfferRepository;
+import com.telefonica.offerengine.Model.ResponseBody;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+
+@Service
+@Transactional
+public class OfferService {
+    @Autowired
+    private OfferRepository repository;
+
+    @Autowired 
+    private LineMobileService lineMobileservice;
+
+    public ResponseEntity<Map<String, Object>> BindingResultErrors(
+        BindingResult bindinResult
+    ) {
+        ResponseBody response = new ResponseBody(
+            bindinResult
+                .getAllErrors()
+                .stream()
+                .findFirst()
+                .get()
+                .getDefaultMessage()
+                .toString(),
+            HttpStatus.INTERNAL_SERVER_ERROR
+        );
+
+        return ResponseEntity.internalServerError().body(response.getResponse());
+    }
+
+    public List<Offer> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Offer> findByIdoffer(int id) {
+        return repository
+            .findByIdoffer(id)
+            .map(mapper -> {
+                return Optional.of(mapper);
+            })
+            .orElseGet(Optional::empty);
+    }
+
+    
+}
