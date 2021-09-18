07da2b51c159edf331f639f100f427ba632afe04
umb.kevsidorov@gmail.com <umb.kevsidorov@gmail.com>
2021-09-17 19:35:06 -0500

    Update offer controllers/cache.

--- src/main/java/com/telefonica/offerengine/Controller/OfferController.java
+++ src/main/java/com/telefonica/offerengine/Controller/OfferController.java
@@ -1,12 +1,17 @@
 package com.telefonica.offerengine.Controller;
 
+import com.telefonica.offerengine.Cache.OffersCache;
+import com.telefonica.offerengine.Data.Offer;
 import com.telefonica.offerengine.Model.OfferFrom;
 import com.telefonica.offerengine.Service.OfferService;
 import java.util.Map;
+import java.util.Optional;
+
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +24,14 @@ public class OfferController {
 
     @Autowired
     private OfferService service;
+
+    @Autowired
+    private OffersCache cache;
+
+    @GetMapping("/cacheByCode/{code}")
+    public ResponseEntity<Optional<Offer>> findByCode(@PathVariable("code") String code) {
+        return ResponseEntity.accepted().body(cache.getOffer(code)); 
+    } 
 
     @PostMapping("/addofferbyline/{id}")
     public ResponseEntity<Map<String, Object>> postMethodName(
@@ -38,4 +51,5 @@ public class OfferController {
             )
             .get();
     }
+    
 }
