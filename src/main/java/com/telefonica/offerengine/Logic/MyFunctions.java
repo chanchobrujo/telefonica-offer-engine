7c1d2d9156738131e49a6b8e1a99fc4938c0cb97
umb.kevsidorov@gmail.com <umb.kevsidorov@gmail.com>
2021-09-17 19:17:58 -0500

    Update formatt string date.

--- src/main/java/com/telefonica/offerengine/Logic/MyFunctions.java
+++ src/main/java/com/telefonica/offerengine/Logic/MyFunctions.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 public class MyFunctions {
 
+    private static String formatString = "dd-MM-yyyy";
+
     public static Date convertStringToDate(String stringdate) {
-        SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat format1 = new SimpleDateFormat(formatString);
         try {
             return format1.parse(stringdate);
         } catch (Exception e) {
@@ -15,13 +17,13 @@ public class MyFunctions {
     }
 
     public static String convertDateToString(Date date) {
-        SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat format1 = new SimpleDateFormat(formatString);
         return format1.format(date);
     }
 
     public static int compareDate(String hora1, String hora2) {
         try {
-            SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
+            SimpleDateFormat format1 = new SimpleDateFormat(formatString);
 
             Date comparar1 = format1.parse(hora1);
             Date comparar2 = format1.parse(hora2);
